8ba063bf16fcb2c062c8291b0c0d4b6e032088f2
I_NotPro <51590857+Clairinz@users.noreply.github.com>
2019-09-05 16:04:37 +0700

    Add files via upload

---
+++ Pertambahan.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <body>
+<title> PERTAMBAHAN </title>
+<?php
+$nilaiPertama = 8 ;
+$nilaiKedua = 2 ;
+$hasil =$nilaiPertama + $nilaiKedua;
+echo $hasil;
+?>
+</body>
+</html>
